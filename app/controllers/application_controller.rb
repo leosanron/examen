1afef8d563e3e29ade2ae33842210ef251d33c83
Leonardo <leoasr12@hotmail.com>
2019-05-02 15:06:05 +0000

    generates-server

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+   # before_action :configure_devise_params, if: :devise_controller?
+    
+    #def configure_permitted_parameters
+    #   devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+   #     user_params.permit(:username, :email)
+        #end
+    #end
+        
+end
